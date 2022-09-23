f6bc1fb12ec02f3b98e39ce02b3339d95ca4a25f
darren4141 <109255883+darren4141@users.noreply.github.com>
2022-09-23 14:55:17 -0400

    Create CCC22S1.java

---
+++ CCC22S1.java
@@ -0,0 +1,36 @@
+//https://dmoj.ca/submission/4656279
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    
+    
+    public static void main(String[] args) throws IOException {  	    	
+    	int n = readInt();
+    	int combos = 0;
+    	
+    	for(int i = 0; i <= n / 4; i++) {
+    		if(n-(4*i) == 0 || (n - (4*i)) % 5 == 0) {
+    			combos++;
+    		}
+    	}
+    	System.out.println(combos);
+    }
+    
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+ 
+    static int readInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+  
+}

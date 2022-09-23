eeaabb9923cd7153648f99fa7ba6eb171e9175d3
darren4141 <109255883+darren4141@users.noreply.github.com>
2022-09-23 14:30:52 -0400

    Create CCC05J5.java

---
+++ CCC05J5.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class main {
+    public static void main(String[] args) throws IOException {
+
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	ArrayList<String> words = new ArrayList<String>();
+    	int c = 0;
+    	
+    	while(true) {
+    		words.add(br.readLine());
+    		if(words.get(c).equals("X")){
+    			words.remove(c);
+    			break;
+    		}
+    		c++;
+
+    	}
+    	
+    	for(int i = 0; i < words.size(); i++) {
+    		
+    		if(!(words.get(i).contains("A"))) {
+    			System.out.println("NO");
+    			continue;
+    		}
+
+	    	while(words.get(i).contains("B") && words.get(i).contains("S")) {
+        			words.set(i, words.get(i).replaceFirst("B", "AN"));
+    				words.set(i, words.get(i).replaceFirst("S", "NA"));    			    		
+	    	}
+	    	
+    		while(words.get(i).contains("AN")) {
+    			words.set(i, words.get(i).replace("AN", ""));
+    			}
+    		
+    		if(words.get(i).equals("A")) {
+    			System.out.println("YES");
+    		}else if (words.get(i).equals(null)){
+    			System.out.println("NO");
+    		}else {
+    			System.out.println("NO");
+    		}    		   	   	    	
+    }    	
+}
+}

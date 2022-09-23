a1b6091145cc77b1773664473a24f8cb7666fd83
darren4141 <109255883+darren4141@users.noreply.github.com>
2022-09-23 14:58:46 -0400

    Create BPC1J3.java

---
+++ BPC1J3.java
@@ -0,0 +1,31 @@
+//https://dmoj.ca/submission/4643172
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException{    	
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    String [] ppl = br.readLine().split(" ");
+    int [] a = new int [n*2];
+    int complaints = 0;
+    
+    for(int i = 0; i < (n*2); i++) {
+    	a[i] = Integer.parseInt(ppl[i]);
+    }
+
+    Arrays.sort(a);
+    
+    for(int i = 0; i < a.length; i+=2) {
+    	complaints += (a[i+1] - a[i]);
+    }
+    
+    System.out.println(complaints);
+    
+ 	}
+   
+}

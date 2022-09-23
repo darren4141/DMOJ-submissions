1063442b3098cb8cf16c22b76cf5be0a5ac6dac7
darren4141 <109255883+darren4141@users.noreply.github.com>
2022-09-23 14:58:17 -0400

    Create CCC00S1.java

---
+++ CCC00S1.java
@@ -0,0 +1,25 @@
+//https://dmoj.ca/submission/4669907
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {    
+    int q = Integer.parseInt(br.readLine()), one = Integer.parseInt(br.readLine()), two = Integer.parseInt(br.readLine()), three = Integer.parseInt(br.readLine()), plays = 0;
+    while(q != 0) {
+    	plays++;
+    	if((plays+2) % 3 == 0) {
+    		q--; one++;
+    		if(one%35 == 0) q+=30;
+    	}else if((plays+1) % 3 == 0) {
+    		q--; two++;
+    		if(two%100 == 0) q+=60;
+    	}else {
+    		q--; three++;
+    		if(three%10 == 0) q+=9;
+    	}
+    }
+    System.out.println("Martha plays " + plays + " times before going broke.");
+    }
+}
